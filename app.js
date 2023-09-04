b1d1f3958ea7603b4e85f1a00246bdd5d7bb8a5f
intheguetto <intheguetto27@gmail.com>
2023-09-04 13:56:20 +0100

    V1

---
+++ app.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  const video = document.getElementById("video");
+  const canvasElement = document.getElementById("canvas");
+  const canvas = canvasElement.getContext("2d");
+  const qrResult = document.getElementById("qr-result");
+  
+
+  function handleSuccess(stream) {
+    video.srcObject = stream;
+    video.play();
+    requestAnimationFrame(tick);
+  }
+
+  function handleError(error) {
+    console.error("Error al acceder a la camara: ", error);
+  }
+
+  function tick() {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+      canvasElement.hidden = false;
+      canvasElement.width = video.videoWidth;
+      canvasElement.height = video.videoHeight;
+      const windowHeight = window.innerHeight;
+      const qrResultHeight = qrResult.offsetHeight;
+      canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+      const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: "dontInvert"
+      });
+      
+
+      if (code) {
+        qrResult.textContent = "Codigo QR detectado: " + code.data;
+        stream.getTracks().forEach(track => track.stop(3000));  // esta linea no funciona por el momento. Deberia reactivar la camara a los 3 seg
+        // Aqui se muestra el codigo leido, añadiriamos el codigo despues si quisieramos hacer algo con el codigo leido
+      } else {
+        //si no lee nada se mantiene este mensaje
+        qrResult.textContent = "Escaneando...";
+        qrResult.style.fontFamily = "Roboto";
+        
+        
+        
+      }
+
+/*Aqui posicionamos y le damos estilo al poceso de escaneo*/
+      if (windowHeight > qrResultHeight) {
+      qrResult.style.position = "fixed";
+      qrResult.style.bottom = "20%";
+      qrResult.style.fontSize = "25px";
+    } else {
+      qrResult.style.position = "relative";
+      qrResult.style.bottom = "auto";
+    }
+  
+    }
+    requestAnimationFrame(tick);
+  }
+
+
+  function startCamera() {
+    // Acceder a la cámara del dispositivo móvil o webcam  (enviroment accede a la camara trasera solamente)
+    navigator.mediaDevices.getUserMedia({ video: {facingMode:"environment"} })
+      .then(handleSuccess)
+      .catch(function(e) {
+        // Si no se puede acceder a la cámara del dispositivo móvil, intentar acceder a la webcam
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+          .then(handleSuccess)
+          .catch(handleError);
+      });
+  }
+
+  const reactivateBtn = document.getElementById("camara");
+
+  // Escuchar evento de clic en el botón
+  reactivateBtn.addEventListener("click", function() {
+    startCamera();
+  });
+
+  startCamera();
+});
+
+
